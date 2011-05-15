f20d9c418e42bdd997b6ac0bc31ea76936356d05
Kiel Hodges <kielhodges@nowhere>
2011-05-14 23:57:05 -0400

    Changed Response implementations to case classes.

--- scala/src/main/scala/replicant/Response.scala
+++ scala/src/main/scala/replicant/Response.scala
@@ -4,10 +4,10 @@ private abstract class Response[Result] {
   def value(fallback: ResponseFallback[Result]): Result
 }
 
-private class ValueResponse[Result](result: Result) extends Response[Result] {
+private case class ValueResponse[Result](result: Result) extends Response[Result] {
   def value(fallback: ResponseFallback[Result]): Result = result
 }
 
-private class UnknownResponse[Result](description: String) extends Response[Result] {
+private case class UnknownResponse[Result](description: String) extends Response[Result] {
   def value(fallback: ResponseFallback[Result]): Result = fallback(description)
 }
--- scala/src/test/scala/replicant/ResponseTest.scala
+++ scala/src/test/scala/replicant/ResponseTest.scala
@@ -12,36 +12,52 @@ import testing._
 class ResponseTest extends junit.JUnit3Suite with ShouldMatchers {
 
   private val result1 = A(1)
+  private val result2 = A(2)
+  private val error1 = "error1"
+  private val error2 = "error2"
 
   @Test def testValueResponseWithNoFallback {
-    val response: Response[A] = new ValueResponse(result1)
+    val response: Response[A] = ValueResponse(result1)
     
     response.value(NoResponse) should be(result1)
   } 
   
   @Test def testValueResponseWithUnitFallback {
     var x = ""
-    val response: Response[Unit] = new ValueResponse(x = "got it")
+    val response: Response[Unit] = ValueResponse(x = "got it")
   
     response.value(UnitFallback)
     x should be("got it")
   } 
-  
-  val errorDescription = "errorDescription"
 
   @Test def testUnknownResponseWithNoFallback {
-    val response: Response[A] = new UnknownResponse(errorDescription)
+    val response: Response[A] = UnknownResponse(error1)
   
     intercept[UnknownResponseException] { 
       response.value(NoResponse)
-    } should equal(new UnknownResponseException(errorDescription))
+    } should equal(new UnknownResponseException(error1))
 
   } 
   
   @Test def testUnknownResponseWithUnitFallback {
-    val response: Response[Unit] = new UnknownResponse(errorDescription)
+    val response: Response[Unit] = UnknownResponse(error1)
     
     response.value(UnitFallback) should be(())
   } 
+
+  @Test def testValueResponseEquality {
+    testEqualityOf(   ValueResponse(result1)  ).
+      shouldEqual(    ValueResponse(result1)  ).
+      shouldNotEqual( ValueResponse(result2)  ).
+      shouldNotEqual( UnknownResponse(error1) )
+  }
+ 
+  @Test def testUnknownResponseEquality {
+    testEqualityOf(   UnknownResponse(error1) ).
+      shouldEqual(    UnknownResponse(error1) ).
+      shouldNotEqual( UnknownResponse(error2) ).
+      shouldNotEqual( ValueResponse(result1)  )
+  }
+  
 
 }
