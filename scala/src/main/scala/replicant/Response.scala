089951d6286f99d29e2367538f85e5cd8cf8481e
Kiel Hodges <kielhodges@nowhere>
2011-05-14 23:32:21 -0400

    Introduced Response.

---
+++ scala/src/main/scala/replicant/Response.scala
@@ -0,0 +1,19 @@
+package replicant
+
+private abstract class Response[Result] {
+
+  def value(fallback: ResponseFallback[Result]): Result
+
+}
+
+private class ValueResponse[Result](value: () => Result) extends Response[Result] {
+  
+  def value(fallback: ResponseFallback[Result]): Result = value()
+
+}
+
+private class UnknownResponse[Result](description: String) extends Response[Result] {
+  
+  def value(fallback: ResponseFallback[Result]): Result = fallback(new UnknownResponseException(description))()
+  
+}
---
+++ scala/src/test/scala/replicant/ResponseTest.scala
@@ -0,0 +1,48 @@
+// Copyright 2011 Kiel Hodges
+package replicant
+
+import org.scalatest._
+import org.scalatest.matchers._
+import org.scalatest.junit.JUnitRunner
+import org.junit._
+import org.junit.runner._
+import testing._
+
+@RunWith(classOf[JUnitRunner])
+class ResponseTest extends junit.JUnit3Suite with ShouldMatchers {
+
+  private val result1 = A(1)
+
+  @Test def testValueResponseWithNoFallback {
+    val response: Response[A] = new ValueResponse(() => result1)
+    
+    response.value(NoResponse) should be(result1)
+  } 
+  
+  @Test def testValueResponseWithUnitFallback {
+    var x = ""
+    val response: Response[Unit] = new ValueResponse(() => x = "got it")
+  
+    response.value(UnitFallback)
+    x should be("got it")
+  } 
+  
+  val errorDescription = "errorDescription"
+
+  @Test def testUnknownResponseWithNoFallback {
+    val response: Response[A] = new UnknownResponse(errorDescription)
+  
+    intercept[UnknownResponseException] { 
+      response.value(NoResponse)
+    } should equal(new UnknownResponseException(errorDescription))
+
+  } 
+  
+  @Test def testUnknownResponseWithUnitFallback {
+    var x = ""
+    val response: Response[Unit] = new UnknownResponse(errorDescription)
+    
+    response.value(UnitFallback) should be(())
+  } 
+  
+}
