d9d3f42281d5b3d7047b5b744a14b7bf14f5a359
Kiel Hodges <kielhodges@nowhere>
2011-10-22 11:39:43 -0400

    Upgraded to Scala 2.9

--- scala/project/build/ScalaReplicantProject.scala
+++ scala/project/build/ScalaReplicantProject.scala
@@ -3,7 +3,7 @@ import sbt._
 class ReplicantProject(info: ProjectInfo) extends DefaultProject(info) {
 
   val scalaToolsSnapshots = ScalaToolsSnapshots
-  val scalatest = "org.scalatest" % "scalatest" % "1.2"          withSources()
-  val junit     = "junit"         % "junit" % "4.8.1"   % "test" withSources()
+  val scalatest = "org.scalatest" %% "scalatest" % "latest.release" withSources()
+  val junit     = "junit"         %  "junit" % "4.8.1"   % "test"   withSources()
 
 }
