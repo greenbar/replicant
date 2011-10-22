163f6167a418217f5fa7b699ed538668385e9f66
Kiel Hodges <kielhodges@nowhere>
2011-10-22 11:57:32 -0400

    Encapsulated ResponseFallback implementations.

--- scala/src/main/scala/replicant/ResponseFallback.scala
+++ scala/src/main/scala/replicant/ResponseFallback.scala
@@ -2,13 +2,13 @@
 package replicant
 
 sealed abstract class ResponseFallback[+Result] {
-  def apply(error: String): Result
+  private[replicant] def apply(error: String): Result
 }
 
-object NoResponse extends ResponseFallback[Nothing] {
+private[replicant] object NoResponse extends ResponseFallback[Nothing] {
   def apply(error: String) = throw new UnknownResponseException(error)
 }
 
-object UnitFallback extends ResponseFallback[Unit] {
+private[replicant] object UnitFallback extends ResponseFallback[Unit] {
   def apply(error: String) { }
 }
